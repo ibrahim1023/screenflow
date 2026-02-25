a29cf86d406c4a1348b06ff5dd6a969d4b75d99e
Ibrahim Arshad <38925737+ibrahim1023@users.noreply.github.com>
2026-02-25 13:40:10 +0400

    Initial Commit

---
+++ screenflow/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  screenflow
+//
+//  Created by Ibrahim Arshad on 2/25/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ screenflowTests/screenflowTests.swift
@@ -0,0 +1,16 @@
+//
+//  screenflowTests.swift
+//  screenflowTests
+//
+//  Created by Ibrahim Arshad on 2/25/26.
+//
+
+import Testing
+
+struct screenflowTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
